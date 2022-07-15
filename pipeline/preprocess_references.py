85b945f51bc5060c4d42796783e8ebbd05cf4245
jbaaijens <>
2022-07-15 22:29:16 +0200

    test preprocessing with fasta index

--- pipeline/preprocess_references.py
+++ pipeline/preprocess_references.py
@@ -12,6 +12,7 @@ def main():
     parser = argparse.ArgumentParser(description="Preprocess reference collection: randomly select samples and write into individual files in lineage-specific directories.")
     parser.add_argument('-m, --metadata', dest='metadata', type=str, help="metadata tsv file for full sequence database")
     parser.add_argument('-f, --fasta', dest='fasta_in', type=str, help="fasta file representing full sequence database")
+    parser.add_argument('-i, --index', dest='fasta_index', type=str, help="fasta index file")
     parser.add_argument('-k', dest='select_k', type=int, default=1000, help="randomly select 1000 sequences per lineage")
     parser.add_argument('--max_N_content', type=float, default=0.001, help="remove genomes with N rate exceeding this threshold")
     parser.add_argument('--min_seq_len', type=int, default=25000, help="remove genomes shorter than this threshold")
@@ -108,46 +109,56 @@ def main():
 
     print("{} sequences selected".format(len(selection_dict.keys())))
     # write sequences to separate files
+    fasta_index = read_index(args.fasta_index)
     print("searching fasta and writing sequences to output directory...")
-    with open(args.fasta_in, 'r') as f_in:
-        keep_line = False
-        line_idx = 0
-        selection_idx = 0
-        for line in f_in:
-            if line[0] == '>':
-                # new record starts here
-                # first store previous record
-                if keep_line:
-                    outfile = "{}/{}/{}.fa".format(args.outdir, lin_id, gisaid_id)
-                    with open(outfile, 'w') as f_out:
-                        f_out.write(">{}\n{}\n".format(seq_id, seq))
-                # now parse new record identifier
-                line_idx += 1
-                if args.verbose and line_idx % 100000 == 0:
-                    print("{} sequences from input fasta processed".format(line_idx))
-                    print("{} sequences from selection found".format(selection_idx))
-                seq_id = line.rstrip('\n').lstrip('>').split('|')[0]
-                # seq_id = line.rstrip('\n').lstrip('>').split()[0]
-                try:
-                    lin_id, gisaid_id = selection_dict[seq_id]
-                    seq = ""
-                    keep_line = True
-                    selection_idx += 1
-                    # now remove key from dict to avoid writing duplicates
-                    del selection_dict[seq_id]
-                except KeyError as e:
-                    # item not found as sequence was not selected
-                    keep_line = False
-            elif keep_line:
-                # append nucleotide sequence
-                seq += line.rstrip('\n')
-        # add final record if necessary
-        if keep_line:
-            outfile = "{}/{}/{}.fa".format(args.outdir, lin_id, gisaid_id)
-            with open(outfile, 'w') as f_out:
-                f_out.write(">{}\n{}\n".format(seq_id, seq))
-        print("{} sequences from input fasta processed".format(line_idx))
-        print("{} sequences from selection found".format(selection_idx))
+    for seq_id, info in selection_dict.items():
+        (lin_id, gisaid_id) = info
+        outfile = "{}/{}/{}.fa".format(args.outdir, lin_id, gisaid_id)
+        [start_idx, end_idx] = fasta_index[seq_id]
+        subprocess.check_call("sed -n '{},{}p;{}q' {} > {}".format(
+                start_idx, end_idx, end_idx+1, args.fasta_in, outfile),
+                shell=True)
+
+    if False:
+        with open(args.fasta_in, 'r') as f_in:
+            keep_line = False
+            line_idx = 0
+            selection_idx = 0
+            for line in f_in:
+                if line[0] == '>':
+                    # new record starts here
+                    # first store previous record
+                    if keep_line:
+                        outfile = "{}/{}/{}.fa".format(args.outdir, lin_id, gisaid_id)
+                        with open(outfile, 'w') as f_out:
+                            f_out.write(">{}\n{}\n".format(seq_id, seq))
+                    # now parse new record identifier
+                    line_idx += 1
+                    if args.verbose and line_idx % 100000 == 0:
+                        print("{} sequences from input fasta processed".format(line_idx))
+                        print("{} sequences from selection found".format(selection_idx))
+                    seq_id = line.rstrip('\n').lstrip('>').split('|')[0]
+                    # seq_id = line.rstrip('\n').lstrip('>').split()[0]
+                    try:
+                        lin_id, gisaid_id = selection_dict[seq_id]
+                        seq = ""
+                        keep_line = True
+                        selection_idx += 1
+                        # now remove key from dict to avoid writing duplicates
+                        del selection_dict[seq_id]
+                    except KeyError as e:
+                        # item not found as sequence was not selected
+                        keep_line = False
+                elif keep_line:
+                    # append nucleotide sequence
+                    seq += line.rstrip('\n')
+            # add final record if necessary
+            if keep_line:
+                outfile = "{}/{}/{}.fa".format(args.outdir, lin_id, gisaid_id)
+                with open(outfile, 'w') as f_out:
+                    f_out.write(">{}\n{}\n".format(seq_id, seq))
+            print("{} sequences from input fasta processed".format(line_idx))
+            print("{} sequences from selection found".format(selection_idx))
 
     # write lineages
     with open("{}/lineages.txt".format(args.outdir), 'w') as f:
@@ -176,6 +187,18 @@ def read_metadata(metadata_file, max_N_content, min_seq_len):
             (df["N-Content"] <= max_N_content) & \
             (df["Sequence length"] >= min_seq_len)]
     return df
+
+
+def read_index(index_tsv):
+    """Read fasta sequence index from file"""
+    print("Reading fasta sequence index from file {}".format(index_tsv))
+    index = {}
+    with open(index_tsv, 'r') as f:
+        for line in f:
+            [seq_id, start_idx, end_idx] = line.rstrip('\n').split('\t')
+            seq_id = seq_id.split('|')[0]
+            index[seq_id] = [int(start_idx), int(end_idx)]
+    return index
 
 
 if __name__ == "__main__":
