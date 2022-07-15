c83d4fd9535bb7f6b0caf7139447e4a050bf3cbd
jbaaijens <>
2022-07-15 10:57:45 +0200

    adding cleanup script to reduce ref dir size

---
+++ pipeline/cleanup_ref_dir.sh
@@ -0,0 +1,18 @@
+# remove everything except the metadata file, the fasta file, the lineage contents file & the kallisto index
+
+REFDIR=$1
+
+for file in ${REFDIR}; do \
+    if [ -d "$file" ] ; then
+        # echo "$file is a directory and it will be removed";
+        rm -r $file
+    fi
+
+    if ! [[ "$file"  =~ ^(${REFDIR}/metadata.tsv|${REFDIR}/sequences.kallisto_idx|${REFDIR}/sequences.fasta|${REFDIR}/lineages.txt)$ ]];
+    then
+        # echo "$file is removed"
+        rm $file
+    fi
+done
+
+echo "Cleanup $REFDIR finished"
