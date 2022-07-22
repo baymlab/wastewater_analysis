0a2261ad150489b118872079fb174fdd4753e704
jbaaijens <>
2022-07-22 16:05:47 +0200

    disable sed option

--- pipeline/preprocess_references.py
+++ pipeline/preprocess_references.py
@@ -26,7 +26,7 @@ def main():
     parser.add_argument('--seed', dest='seed', default=0, type=int, help="random seed for sequence selection")
     parser.add_argument('-o, --outdir', dest='outdir', type=str, default="seqs_per_lineage", help="output directory")
     parser.add_argument('--verbose', action='store_true')
-    parser.add_argument('--test_sed', action='store_true')
+    # parser.add_argument('--test_sed', action='store_true')
     args = parser.parse_args()
 
     # create output directory
@@ -114,6 +114,7 @@ def main():
     # write sequences to separate files
     fasta_index = read_index(args.fasta_index)
     print("searching fasta and writing sequences to output directory...")
+    args.test_sed = False
     if args.test_sed:
         for seq_id, info in selection_dict.items():
             (lin_id, gisaid_id) = info
